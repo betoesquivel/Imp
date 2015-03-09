708d1d7652380e9bd4033e645f40c44a31d60859
Beto Esquivel <ppbeto94@gmail.com>
2015-03-08 21:09:12 -0600

    Added the main usingTheDict.txt file with the algorithm

---
+++ ejemploHash.py
@@ -0,0 +1,11 @@
+maindictionary = {
+    'global' : {
+        'cont' : { 'type' : 'int' , 'dimensionx': '0',  'dimensiony':'0'},
+        'cont2' : { 'type' : 'int' , 'dimensionx': '0', 'dimensiony':'0'},
+    },
+    'algo' : {
+        'a' : { 'type' : 'int' , 'dimensionx': '0',  'dimensiony':'0'}
+    },
+    'main' : {
+    }
+}
